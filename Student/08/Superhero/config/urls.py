c2b3ee5c1c40f2fc030fdef1e3aeebd7ab4f939f
Matthew-Lessman <melbud282@gmail.com>
2024-12-06 08:59:57 -0700

    Proj 8

---
+++ Student/08/Superhero/config/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, include
+from hero.views import HeroCreateView, HeroDeleteView, HeroDetailView, HeroListView, HeroUpdateView, SignUpView
+from django.contrib import admin
+from django.views.generic import RedirectView
+
+urlpatterns = [
+
+    # Hero
+    path('',                HeroListView.as_view(),    name='hero_list'),
+    path('<int:pk>',        HeroDetailView.as_view(),  name='hero_detail'),
+    path('add',             HeroCreateView.as_view(),  name='hero_add'),
+    path('<int:pk>/',       HeroUpdateView.as_view(),  name='hero_edit'),
+    path('<int:pk>/delete', HeroDeleteView.as_view(),  name='hero_delete'),
+
+
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', SignUpView.as_view(), name='signup'),
+
+
+    path('admin/', admin.site.urls),
+
+
+]
